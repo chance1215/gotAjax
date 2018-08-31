b83f9d69c123413bcea33fed234b4a693683cd2e
Chelsea Grimmett <c.grimmett@me.com>
2018-08-30 21:02:46 -0700

    something isnt working

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-const baseURL =" https://anapioficeandfire.com/Documentation"
-const character ='https://anapioficeandfire.com/Documentation#characters'
-const house = 'https://anapioficeandfire.com/Documentation#houses'
-const firstFavorite = document.querySelectorAll('.favorites')
+const character ='https://anapioficeandfire.com/api/characters';
+const house = 'https://anapioficeandfire.com/api/houses';
+const firstFavorite = document.querySelectorAll('.favorites');
+const theirHouse = document.querySelectorAll('.houses')
 
 
 
 
-firstFavorite.
 
-axios.get(`${character}`)
+axios.get(`${character}/name`)
 .then(function(response){
   console.log(response)
 
-  }
-  
-
-  firstFavorite.innerHTML = character.value
-
 })
+
+
+  //firstFavorite.innerHTML = character.value.innerHTML
