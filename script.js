64bb4fa6a7e01ee9b399a452e7619f2e73745122
Chelsea Grimmett <c.grimmett@me.com>
2018-08-30 20:11:05 -0700

    html setup

---
+++ script.js
@@ -0,0 +1,20 @@
+const baseURL =" https://anapioficeandfire.com/Documentation"
+const character ='https://anapioficeandfire.com/Documentation#characters'
+const house = 'https://anapioficeandfire.com/Documentation#houses'
+const firstFavorite = document.querySelectorAll('.favorites')
+
+
+
+
+firstFavorite.
+
+axios.get(`${character}`)
+.then(function(response){
+  console.log(response)
+
+  }
+  
+
+  firstFavorite.innerHTML = character.value
+
+})
